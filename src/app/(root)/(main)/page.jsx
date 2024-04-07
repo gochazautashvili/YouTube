bec971b0efcafe13cb87713e9aa91f7b51d45447
Gocha <gochazautashvili2017@gmail.com>
2024-04-07 20:42:40 +0400

    new

--- src/app/(root)/(main)/page.jsx
+++ src/app/(root)/(main)/page.jsx
@@ -1,9 +1,12 @@
 import Hero from "@components/Hero";
+import { Suspense } from "react";
 
 const Home = () => {
   return (
     <main className="mt-20 lg:ml-[250px] px-6 pt-4">
-      <Hero />
+      <Suspense fallback="Loading...">
+        <Hero />
+      </Suspense>
     </main>
   );
 };
