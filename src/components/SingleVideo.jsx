e954d0a5251c9e0306db7a96112431cd76d44150
Gocha <gochazautashvili2017@gmail.com>
2024-04-07 21:04:10 +0400

    new

--- src/components/SingleVideo.jsx
+++ src/components/SingleVideo.jsx
@@ -6,9 +6,9 @@ import { QFormat } from "@lib/FormatQuantity";
 const SingleVideo = ({ image, title, name, createdAt, id, views }) => {
   return (
     <div className="flex gap-x-3">
-      <Link className="w-[170px] h-[100px]" href={`/video/${id}`}>
+      <Link className="w-[170px] h-[100px] min-w-[160px]" href={`/video/${id}`}>
         <Image
-          className="rounded-xl w-auto h-full"
+          className="rounded-xl w-full h-full"
           src={image}
           alt="single-video-by-category"
           width={170}
@@ -16,7 +16,7 @@ const SingleVideo = ({ image, title, name, createdAt, id, views }) => {
         />
       </Link>
       <Link href={`'/video/${id}`} className="flex flex-col">
-        <h1 className="font-medium text-black">{title}</h1>
+        <h1 className="text-sm sm:text-base font-medium text-black">{title}</h1>
         <p className="mt-1 text-sm text-gray-600">{name}</p>
         <p className="text-[12px] text-gray-500 mt-1">
           {QFormat(views)} views . {moment(createdAt).fromNow()}
