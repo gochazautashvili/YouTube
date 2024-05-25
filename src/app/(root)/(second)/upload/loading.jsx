c6fdf154dd867c0007cd730156ff1e403fdce3c6
Gocha <gochazautashvili2017@gmail.com>
2024-05-25 21:35:54 +0400

    add loading

---
+++ src/app/(root)/(second)/upload/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="absolute w-full h-screen bg-gray-800 flex items-center justify-center">
+      <h1 className="text-white text-2xl">Loading...</h1>
+    </div>
+  );
+};
+
+export default loading;
--- src/app/(root)/(second)/upload/page.jsx
+++ src/app/(root)/(second)/upload/page.jsx
@@ -1,4 +1,10 @@
-import Upload from "@components/Upload";
+import dynamic from "next/dynamic";
+const Upload = dynamic(() => import("@components/Upload"), {
+  ssr: false,
+  loading: () => (
+    <h1 className="text-center text-3xl text-black">Loading...</h1>
+  ),
+});
 
 const Uploading = () => {
   return (
